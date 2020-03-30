298d5a17805a4445827e14fad8ce89c21210be75
c-grattan <56635397+c-grattan@users.noreply.github.com>
2020-03-30 22:18:26 +0100

    Add new name type

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
-var singular = ["Lord", "Wolf", "Pickle"];
-var plural = ["Lords", "Wolves", "Pickles"];
+var singular = [];
+var plural = [];
+
+function addWord(one, multi)
+{
+	singular.push(one);
+	plural.push(multi);
+}
+
+addWord("Lord", "Lords");
+addWord("Wolf", "Wolves");
+addWord("Pickle", "Pickles");
+
+function randomItem(list)
+{
+	return list[Math.floor(Math.random() * list.length)]
+}
 
 function generateContent()
 {
 	var name = document.getElementById("name");
-	name.innerHTML = singular[Math.floor(Math.random() * singular.length)] + " of " + plural[Math.floor(Math.random() * plural.length)];
+	switch(Math.floor(Math.random() * 2))
+	{
+		case 0:
+			name.innerHTML = randomItem(singular) + " of " + randomItem(plural);
+			break;
+			
+		case 1:
+			name.innerHTML = (10 ** (Math.floor(Math.random() * 10) + 1)) + " " + randomItem(plural);
+			break;
+			
+		default:
+		console.log("Incorrect random number generated for name type");
+			break;
+	}
+	
 }
